e907e6842363fd5e0df9bc22fdeb9a9163bbc39b
azuredevops <86014748+Navaltechzure3@users.noreply.github.com>
2021-09-22 14:17:46 +0000

    Add files via upload

---
+++ win.tf
@@ -0,0 +1,87 @@
+provider "azurerm" {
+  features {}
+subscription_id = "2c49c8bd-c814-49c5-b962-409ba3b0876e"
+client_id       = "d985df0d-a8d3-4d69-80e3-bc7c576d20e8"
+client_secret   = "4zh7Q~t4OTa-ay_1f1PmQTcKXd6zu4ISTFMEv"
+tenant_id       = "4f008545-7eb1-44e9-b7f4-ea0f30b4e798"
+
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "challengelab"
+  location = "US-East-2"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B2ms"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "vm_extension_install_iis" {
+  name                       = "vm_extension_install_iis"
+  virtual_machine_id         = azurerm_windows_virtual_machine.example.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.8"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
+    }
+SETTINGS
+
+protected_settings = <<PROTECTED_SETTINGS
+    {
+        "commandToExecute": "myExecutionCommand",
+        "storageAccountName": "myStorageAccountName",
+        "storageAccountKey": "myStorageAccountKey",
+        "managedIdentity" : {}
+    }
+PROTECTED_SETTINGS
+}
